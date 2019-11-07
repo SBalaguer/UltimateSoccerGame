c48e0e60b0e07ea3e3d15f7adcbad0b6d37b0b61
Santi Balaguer <santiago.balaguer@gmail.com>
2019-11-07 18:38:06 +0000

    thursday night

--- javascript/index-script.js
+++ javascript/index-script.js
@@ -2,7 +2,7 @@ const $canvas = document.querySelector("canvas")
 const context = $canvas.getContext('2d')
 
 const sprite = new Image()
-sprite.src = "../images/sprite-inicial.png"
+sprite.src = "./images/sprite-inicial.png"
 //sprite.src = "file:///Users/santiagoebalaguer/ironhack/projects/game-project/images/sprite-inicial.png"
 const arrImg = [[217,263],[4,3],[217,3],[430,3],[643,3],[4,133],[217,133]]
 let i = 0
